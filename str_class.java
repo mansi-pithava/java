25b851cf3a8971394e8a30343290632af6e2c0cc
mansi-pithava <kaushalpithava2301@gmail.com>
2023-12-29 16:16:02 +0530

    new

---
+++ str_class.java
@@ -0,0 +1,9 @@
+class str_class{
+    public static void main(String[] args) {
+        {
+            String s="java is programming language";
+            String s1="hello  !";
+            System.out.println("length="+s+"  "+s.length()+"s1="+s1+" "+s1.length());
+        }
+    }
+}
