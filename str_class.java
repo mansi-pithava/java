7ff3384df643c6de6d2dd935a9fe23fc0c1266f4
mansi-pithava <kaushalpithava2301@gmail.com>
2023-12-29 16:21:29 +0530

    update

--- str_class.java
+++ str_class.java
@@ -2,8 +2,13 @@ class str_class{
     public static void main(String[] args) {
         {
             String s="java is programming language";
-            String s1="hello  !";
-            System.out.println("length="+s+"  "+s.length()+"s1="+s1+" "+s1.length());
+            String s1="  hello  !";
+            System.out.println("length  "+s+"  "+s.length()+"  s1=  "+s1+" "+s1.length());
+            System.out.println("trim"+s1.trim());
+            System.out.println("concate"+(s1+s));
+            System.out.println("concate()"+s1.concat(s));
+           
+           System.out.println("trim"+s1.equals(s));
         }
     }
 }
