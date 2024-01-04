9a63c1a3184f29cdfe1e06ee5f338cf48aeb6222
mansi-pithava <154572316+mansi-pithava@users.noreply.github.com>
2024-01-04 11:45:36 +0530

    practice
    practice of java programming

---
+++ pattern.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class pattern 
+{
+    public static void main(String[] args)
+     {
+        int i, j, no;
+        Scanner s=new Scanner(System.in);
+        for (i = 0; i <= 5; i++) 
+        {
+            for (j = 1; j <= i; j++) 
+            {
+                    //System.out.print(j);//1  :-i,1,22,333,4444,55555
+            } 
+                    System.out.println();//0
+               
+        }
+           // System.out.println(" natural number");
+            System.out.println("how many odd/even/square number print between range enter please");
+            no=s.nextInt();
+                           //10
+            for(int n=1;n<=no;n++)
+            {
+                System.out.println(n+" ="+n*n);
+                if(n%2==0)
+                {
+                   // System.out.println(" "+n);//even number
+
+                }
+                else
+                {
+                 // System.out.print(n+",");//odd number
+
+                }
+            }
+            
+        
+     }
+
+    }
+
+
