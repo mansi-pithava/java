de3f3b0ba9bc6e4abc06b9e7a65aac200c62cdff
mansi-pithava <kaushalpithava2301@gmail.com>
2024-01-17 11:27:11 +0530

    dlt

--- pkg/a.java
+++
@@ -1,8 +0,0 @@
-package pkg;
-
-public class a {
- public void f1()
- {
-    System.out.println("f1 function call in package ");
- }   
-}
--- pkg/b.java
+++
@@ -1,9 +0,0 @@
-package pkg;
-
-public class b {
-    public void f2()
-    {
-        System.out.println("f2 function called");
-    }
-    
-}
