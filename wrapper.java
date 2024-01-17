7128be6c21d286146c0935e2c32a98bc485aa91e
mansi-pithava <kaushalpithava2301@gmail.com>
2024-01-17 08:29:12 +0530

    newv add

---
+++ wrapper.java
@@ -0,0 +1,7 @@
+public class wrapper {
+    public static void main(String[] args) {
+        int a=10;
+        Integer ob=Integer.valueOf(a);
+        System.out.println("a="+a+"object="+ob);
+    }
+}
