016087dba33f9989098c5f6faca048cca8f99707
mansi-pithava <kaushalpithava2301@gmail.com>
2024-02-19 16:30:35 +0530

    add

---
+++ app.java
@@ -0,0 +1,14 @@
+import java.applet.*;
+import java.awt.*;
+/* <applet code="app.class" width="300" height="300">
+</applet> 
+*/
+public class app extends Applet{
+    public void paint(Graphics g)
+    {
+        g.setColor(Color.BLACK);
+        
+        g.drawString("Parangat College Of Computer Science - halvad",50,50);
+    }
+}
+ 
---
+++ my.java
@@ -0,0 +1,12 @@
+import java.applet.*;
+import java.awt.*;
+/*<applet code="my.class" width=300 height=300>
+</applet>*/
+public class my extends Applet
+{
+	public void paint(Graphics g)
+	{	
+		g.setColor(Color.BLACK);
+		g.drawString("rAm",50,50);
+	}
+}
